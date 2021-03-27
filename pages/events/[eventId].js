7140d88bbe391fe038981db3e42229bd0cc0227c
Michael Isprihanto <michael.isprihanto@gmail.com>
2021-03-27 18:10:18 +0700

    Initial commit

---
+++ pages/events/[eventId].js
@@ -0,0 +1,34 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+
+const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if(!event) {
+    return (
+      <ErrorAlert>
+        <p>Event not found.</p>
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} location={event.location} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  )
+}
+
+export default EventDetailPage;
