04ed6e75dec2ed9bee4eaae02fe2877fb3710f44
michaelisprihanto <michael.isprihanto@gmail.com>
2021-04-11 22:45:58 +0700

    Integrated firebase

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,22 +1,18 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+const EventDetailPage = (props) => {
+  const event = props.selectedEvent;
 
   if(!event) {
     return (
-      <ErrorAlert>
-        <p>Event not found.</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -29,6 +25,28 @@ const EventDetailPage = () => {
       </EventContent>
     </Fragment>
   )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    }, 
+    revalidate: 600
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: 'blocking'
+  }
 }
 
 export default EventDetailPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { Fragment } from "react";
 import { useRouter } from 'next/router';
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  const events = props.events;
   const router = useRouter();
 
   const findEventsHandler = (year, month) => {
@@ -19,6 +19,16 @@ const AllEventsPage = () => {
       <EventList items={events} />
     </Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 600
+  }
 }
 
 export default AllEventsPage;
